91cc985339e17d0441c74a2ab8d388400ff7510e
damiri127 <damiricepri127@gmail.com>
2022-05-23 17:21:16 +0700

    hitung balok by ncep

---
+++ latihan/hitung_balok.py
@@ -0,0 +1,13 @@
+from balok import Balok
+
+class Balok:
+    
+    def hitung_luas(self,balok: Balok):
+        return 2 * (balok.get_panjang() + balok.get_lebar() + balok.get_tinggi)
+    
+    def hitung_keliling(self, balok: Balok):
+        return 4 * (balok.get_panjang + balok.get_lebar() + balok.get_tinggi())
+    
+    def hitung_volume(self, balok: Balok):
+        return balok.get_panjang() + balok.get_lebar() + balok.get_tinggi()
+    
