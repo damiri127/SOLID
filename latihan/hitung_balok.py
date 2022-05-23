34147acffd1fff86d5bb447d3022d2f4300e8932
damiri127 <damiricepri127@gmail.com>
2022-05-23 17:42:51 +0700

    perubahan 2

--- latihan/hitung_balok.py
+++ latihan/hitung_balok.py
@@ -3,7 +3,7 @@ from balok import Balok
 class HitungBalok:
     
     def hitung_luas(self,balok: Balok):
-        return 2 * (balok.get_panjang() + balok.get_lebar() + balok.get_tinggi)
+        return 2 * (balok.get_panjang() + balok.get_lebar() + balok.get_tinggi())
     
     def hitung_keliling(self, balok: Balok):
         return 4 * (balok.get_panjang + balok.get_lebar() + balok.get_tinggi())
